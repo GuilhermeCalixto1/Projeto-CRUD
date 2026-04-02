f794f95d84b7ef477e4c3c4cc0834b53e057e79b
Guilherme dos Santos Calixto <guilhermegbm.gc@gmail.com>
2026-04-02 18:49:36 +0000

    Refatora configuração do Vite para melhor organização e legibilidade

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite';
-
+import { resolve } from 'path';
 
 export default defineConfig({
-    server:{
-        port:3001
-    }
+  server: {
+    port: 3001
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        cadastrar: resolve(__dirname, 'src/pages/cadastrar.html'),
+        consultar: resolve(__dirname, 'src/pages/consultar.html'),
+        alterar: resolve(__dirname, 'src/pages/alterar.html'),
+        excluir: resolve(__dirname, 'src/pages/excluir.html'),
+      },
+    },
+  },
 });
